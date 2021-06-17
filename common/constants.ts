92cd446ef234bfb31320b65ab3c5524eeb8b3488
muertomariachi <muertomariachi@gmail.com>
2021-06-17 18:48:45 -0400

    added url constants, refatored home index, pulled out full page loading component

---
+++ common/constants.ts
@@ -0,0 +1,6 @@
+export const AUDIT_REPORT_URL = 'https://code423n4.com/reports/2021-04-basedloans';
+export const ETHERSCAN_URL = 'https://etherscan.io/token/0x68481f2c02be3786987ac2bc3327171c5d05f9bd';
+export const DISCORD_URL = 'https://discord.gg/H44DWX48Ra';
+export const TWITTER_URL = 'https://twitter.com/basedloans';
+export const TELEGRAM_URL = 'https://t.me/basedmoney';
+export const GITHUB_URL = 'https://github.com/Based-Loans'
